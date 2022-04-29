e13a892e1d7b312f407329059423557d15a977f7
Aris Kyriakopoulos <ariskyr@gmail.com>
2022-04-29 12:22:59 +0300

    Last commit of main

--- main.py
+++ main.py
@@ -1,16 +1,18 @@
-from pyclbr import Function
-import pandas as pd
 import numpy as np
 import os
 import tensorflow as tf
 from tensorflow import keras
 from tensorflow.keras import layers
+from tensorflow.keras.regularizers import l2
+from tensorflow.keras.callbacks import EarlyStopping, ModelCheckpoint
 import re
 from matplotlib import pyplot as plt
 from sklearn.model_selection import KFold
 import Preprocess as pp
 
 #tf config
+physical_devices = tf.config.list_physical_devices('GPU')
+tf.config.experimental.set_memory_growth(physical_devices[0], True)
 os.environ['TF_CPP_MIN_LOG_LEVEL'] = '2'
 
 #########
@@ -33,11 +35,11 @@ X_data = np.stack(X_data)
 Y_data = np.stack(Y_data)
 
 # Model configuration
-batch_size = 32
+batch_size = 16
 loss_function = tf.keras.losses.BinaryCrossentropy(from_logits=False)
 no_classes = 20
-no_epochs = 100
-optimizer = tf.keras.optimizers.SGD(learning_rate=0.001)
+no_epochs = 200
+optimizer = tf.keras.optimizers.SGD(learning_rate=0.1, momentum=0.6)
 verbosity = 2
 num_folds = 5
 
@@ -53,32 +55,40 @@ kfold = KFold(n_splits=num_folds, shuffle=True)
 fold = 1
 for train,test in kfold.split(X_data, Y_data):
     #architecture
-    inputs = keras.Input(shape=(8520))
-    dense1 = layers.Dense(20,activation='relu')(inputs)
-    outputs = layers.Dense(no_classes,activation='sigmoid')(dense1)
+    inputs = keras.Input(shape=(8520,))
+    x = layers.Dense(40,activation='relu', kernel_regularizer=l2(0.5))(inputs)
+    x = layers.Dense(40,activation='relu', kernel_regularizer=l2(0.5))(x)
+    outputs = layers.Dense(no_classes,activation='sigmoid')(x)
     model = keras.Model(inputs=inputs,outputs=outputs,name='DeliciousMIL_model')
 
+    print(model.summary())
     #compile model
     model.compile(loss=loss_function,
                 optimizer=optimizer,
-                metrics=['accuracy','mse'],
+                metrics=['accuracy', 'mse'],
                 )
     
     print('-----------------------------------------------------------------------------')
     print(f'Training for fold {fold} ...')
 
+    es = EarlyStopping(monitor='val_loss', mode='min', verbose=1, patience=25)
+    mc = ModelCheckpoint('modelA4a.h5', monitor='val_accuracy', mode='max', verbose=0, save_best_only=True)
+    
     history = model.fit(X_data[train], Y_data[train],
                         batch_size=batch_size,
                         epochs=no_epochs,
-                        verbose=verbosity)
-
+                        verbose=verbosity,
+                        validation_data=(X_data[test], Y_data[test]),
+                        callbacks=[es,mc])
+    
     scores = model.evaluate(X_data[test], Y_data[test], verbose=2)
     print(f'Score for fold {fold}: {model.metrics_names[0]} of {scores[0]}; {model.metrics_names[1]} of {scores[1]*100}%; {model.metrics_names[2]} of {scores[2]}.')
     loss_per_fold.append(scores[0])
     acc_per_fold.append(scores[1]*100)
     mse_per_fold.append(scores[2])
-
+    
     fold += 1
+
 
 # == Provide average scores ==
 print('------------------------------------------------------------------------')
@@ -92,4 +102,33 @@ print(f'> Accuracy: {np.mean(acc_per_fold)} (+- {np.std(acc_per_fold)})')
 print(f'> Loss: {np.mean(loss_per_fold)}')
 print(f'> MSE: {np.mean(mse_per_fold)}')
 print('------------------------------------------------------------------------')
+
+# == Plot model history accurracy ==
+plt.plot(history.history['accuracy'])
+plt.plot(history.history['val_accuracy'])
+plt.title('model accuracy')
+plt.ylabel('accuracy')
+plt.xlabel('epoch')
+plt.legend(['train','test'], loc='upper left')
+plt.show()
+
+# == Plot model history CE loss ==
+plt.plot(history.history['loss'])
+plt.plot(history.history['val_loss'])
+plt.title('model loss')
+plt.ylabel('loss')
+plt.xlabel('epoch')
+plt.legend(['train','test'], loc='upper left')
+plt.show()
+
+# == Plot model history MSE ==
+plt.plot(history.history['mse'])
+plt.plot(history.history['val_mse'])
+plt.title('model MSE')
+plt.ylabel('MSE')
+plt.xlabel('epoch')
+plt.legend(['train','test'], loc='upper left')
+plt.show()
+
+
 x= 0
---
+++ unitTesting.py
@@ -0,0 +1,22 @@
+# Imports
+import tensorflow as tf
+from sklearn.datasets import make_multilabel_classification
+from sklearn.model_selection import train_test_split
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.layers import Dense
+from tensorflow.keras.losses import binary_crossentropy
+from tensorflow.keras.optimizers import Adam
+
+def paramchange():
+  image = tf.placeholder(tf.float32, (None, 100, 100, 3))
+  model = Model(image)
+  sess = tf.Session()
+  sess.run(tf.global_variables_initializer())
+  before = sess.run(tf.trainable_variables())
+  _ = sess.run(model.train, feed_dict={
+               image: np.ones((1, 100, 100, 3)),
+               })
+  after = sess.run(tf.trainable_variables())
+  for b, a, n in zip(before, after):
+      # Make sure something changed.
+      assert (b != a).any()
